d805c3b6a4f37f51fc4378d16cacc438841a2081
Yun Min Woo <77202633+ymw0407@users.noreply.github.com>
2022-12-19 04:55:23 +0900

    Update PDcontrol.ino

--- PDcontrol.ino
+++ PDcontrol.ino
@@ -15,7 +15,7 @@
 #define _EMA_ALPHA 0.8       // EMA weight of new sample (range: 0 to 1)
 
 // Servo adjustment
-#define _DUTY_NEU 1400  // Servo angle: 0 degree
+#define _DUTY_NEU 1200  // Servo angle: 0 degree
 #define _DUTY_MAX 2000  // Servo angle: D degree
 #define _DUTY_MIN 600   // Servo angle: E degree
 #define _SERVO_ANGLE_DIFF 85 // Replace with |D - E| degree
